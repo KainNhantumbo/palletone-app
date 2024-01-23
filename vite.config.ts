93898c5339456d59f6c22e45b08e2c4d3b24ee62
Kain Nhantumbo <nhantumbok@gmail.com>
2024-01-23 14:20:11 +0200

    feat: added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
-import path from 'node:path';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import electron from 'vite-plugin-electron/simple';
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import electron from "vite-plugin-electron/simple";
+import { VitePWA } from "vite-plugin-pwa";
+import Package from "./package.json";
 
 export default defineConfig({
   server: { port: 3000 },
@@ -11,12 +13,69 @@ export default defineConfig({
     tsconfigPaths(),
     electron({
       main: {
-        entry: 'electron/main.ts'
+        entry: "electron/main.ts"
       },
       preload: {
-        input: path.join(__dirname, 'electron/preload.ts')
+        input: path.join(__dirname, "electron/preload.ts")
       },
       renderer: {}
+    }),
+    VitePWA({
+      mode: "production",
+      disable: false,
+      registerType: "prompt",
+      includeAssets: [],
+      minify: true,
+      manifest: {
+        name: Package.name,
+        short_name: Package.name,
+        description: Package.description,
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "icon"
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "icon"
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "icon"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        orientation: "portrait"
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: "*",
+            method: "GET",
+            handler: "CacheFirst" as const,
+            options: {
+              cacheName: Package.name.concat("-cache"),
+              cacheableResponse: { statuses: [200] }
+            }
+          }
+        ]
+      }
     })
   ]
 });
