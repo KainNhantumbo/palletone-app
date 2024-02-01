65227720009c7b73baa04923b1548daaf3f66532
Kain Nhantumbo <nhantumbok@gmail.com>
2024-02-01 22:28:22 +0200

    feat: added support for rendering, copying and deleting saved tetradic colors

--- src/routes/saved-colors.tsx
+++ src/routes/saved-colors.tsx
@@ -23,6 +23,7 @@ import {
   CopyIcon,
   Dice2Icon,
   Dice3Icon,
+  Dice4Icon,
   DicesIcon,
   DropletIcon,
   Layers2Icon,
@@ -155,7 +156,7 @@ export default function SavedColors() {
         <Separator decorative />
 
         <Tabs defaultValue="solid" className="w-full px-2">
-          <TabsList className="mx-auto mb-3 grid w-fit grid-cols-6 place-content-center place-items-center gap-8 bg-background-default">
+          <TabsList className="mx-auto mb-3 grid w-fit grid-cols-8 place-content-center place-items-center gap-8 bg-background-default">
             <TabsTrigger value="solid" className="rounded-3xl">
               <div className="group mx-auto flex w-full max-w-[200px] items-center gap-1 rounded-full">
                 <DropletIcon className="w-[18px] transition-colors group-hover:stroke-blue-400" />
@@ -201,6 +202,22 @@ export default function SavedColors() {
                 <Dice3Icon className="w-[18px] transition-colors group-hover:stroke-blue-400" />
                 <span className="font-semibold transition-colors group-hover:text-blue-400">
                   Triadic
+                </span>
+              </div>
+            </TabsTrigger>
+            <TabsTrigger value="tetradic" className="rounded-3xl">
+              <div className="group mx-auto flex w-full max-w-[200px] items-center gap-1 rounded-3xl">
+                <Dice4Icon className="w-[18px] transition-colors group-hover:stroke-blue-400" />
+                <span className="font-semibold transition-colors group-hover:text-blue-400">
+                  Tetradic
+                </span>
+              </div>
+            </TabsTrigger>
+            <TabsTrigger value="monochromatic" className="rounded-3xl">
+              <div className="group mx-auto flex w-full max-w-[200px] items-center gap-1 rounded-3xl">
+                <Dice4Icon className="w-[18px] transition-colors group-hover:stroke-blue-400" />
+                <span className="font-semibold transition-colors group-hover:text-blue-400">
+                  Monochromatic
                 </span>
               </div>
             </TabsTrigger>
@@ -574,6 +591,69 @@ export default function SavedColors() {
                 <EmptyMessage
                   icon={Layers3Icon}
                   message="Your saved triadic colors will appear here. Collect and save some triadic colors to start."
+                />
+              ) : null}
+            </section>
+          </TabsContent>
+
+          <TabsContent value="tetradic">
+            <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
+              <h3>About {harmonyColorsDB.tetradic.length} tetradic colors saved.</h3>
+              <Separator decorative className="mb-2" />
+
+              {harmonyColorsDB.tetradic.length > 0 ? (
+                <section className="grid w-full grid-cols-1 gap-2 mobile:grid-cols-2 md:grid-cols-3 md:flex-row md:gap-3">
+                  {harmonyColorsDB.tetradic
+                    .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+                    .map(({ id, createdAt, originalColor, values }, i) => (
+                      <motion.div
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        whileHover={{ y: -10, transition: { delay: 0 } }}
+                        className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
+                        key={id}>
+                        <div className="base-shadow base-border base-shadow base-border relative grid max-h-[540px] min-h-[200px] w-full grid-cols-5 overflow-clip rounded-2xl md:w-full md:max-w-[220px] lg:max-w-[320px]">
+                          {[...values, originalColor].map((value, i) => (
+                            <Fragment key={i}>
+                              <div
+                                style={{
+                                  background: tinycolor(value).toRgbString()
+                                }}
+                                className="relative min-h-40 w-full">
+                                <span
+                                  className={cn(
+                                    'base-border absolute left-2 top-2 h-fit w-fit rounded-full bg-background-default p-1 text-xs font-bold',
+                                    { ['hidden']: i > 3 }
+                                  )}>
+                                  {i > 3 ? '' : `0${i + 1}`}
+                                </span>
+                              </div>
+                            </Fragment>
+                          ))}
+                        </div>
+
+                        <div className="flex w-full items-center justify-between gap-1 px-2">
+                          <p className="text-xs font-medium">{getDate(createdAt)}</p>
+                          <div className="flex items-center gap-1">
+                            <RemoveColorAlert
+                              onConfirm={() => handleRemoveTetradicColor(id)}
+                            />
+                            <GenericColorsMenu
+                              title="Tetradic"
+                              originalColor={originalColor}
+                              values={values}
+                            />
+                          </div>
+                        </div>
+                      </motion.div>
+                    ))}
+                </section>
+              ) : null}
+
+              {harmonyColorsDB.tetradic.length < 1 ? (
+                <EmptyMessage
+                  icon={Layers3Icon}
+                  message="Your saved tetradic colors will appear here. Collect and save some tetradic colors to start."
                 />
               ) : null}
             </section>
