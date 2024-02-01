ca053b3e33e60364f64c74e390fd6875b4e9401d
Kain Nhantumbo <nhantumbok@gmail.com>
2024-02-01 22:46:14 +0200

    feat: added support for rendering, copying and deleting saved chroma colors

--- src/routes/saved-colors.tsx
+++ src/routes/saved-colors.tsx
@@ -1,5 +1,4 @@
 import {
-  copyToClipboard,
   normalizeColorOutput,
   buildGradient,
   getDate,
@@ -20,7 +19,6 @@ import {
   ArrowLeftIcon,
   BirdIcon,
   CandyCaneIcon,
-  CopyIcon,
   Dice2Icon,
   Dice3Icon,
   Dice4Icon,
@@ -225,7 +223,7 @@ export default function SavedColors() {
 
           <TabsContent value="solid">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>About {solidColorsDB.length} colors saved.</h3>
+              <h3>About {solidColorsDB.length} solid colors.</h3>
               <Separator decorative className="mb-2" />
 
               {solidColorsDB.length > 0 ? (
@@ -235,7 +233,7 @@ export default function SavedColors() {
                     .map(({ id, value, createdAt }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -275,7 +273,7 @@ export default function SavedColors() {
 
           <TabsContent value="gradient">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>About {gradientColorsDB.length} gradients saved.</h3>
+              <h3>About {gradientColorsDB.length} gradient colors.</h3>
               <Separator decorative className="mb-2" />
 
               {gradientColorsDB.length > 0 ? (
@@ -285,7 +283,7 @@ export default function SavedColors() {
                     .map(({ id, color_1, color_2, CSSGradient, createdAt }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -340,9 +338,7 @@ export default function SavedColors() {
 
           <TabsContent value="complement">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>
-                About {harmonyColorsDB.complement.length} complement colors saved.
-              </h3>
+              <h3>About {harmonyColorsDB.complement.length} complement colors.</h3>
               <Separator decorative className="mb-2" />
 
               {harmonyColorsDB.complement.length > 0 ? (
@@ -352,7 +348,7 @@ export default function SavedColors() {
                     .map(({ id, createdAt, originalColor, value }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -404,9 +400,7 @@ export default function SavedColors() {
 
           <TabsContent value="analogous">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>
-                About {harmonyColorsDB.analogous.length} analogous colors saved.
-              </h3>
+              <h3>About {harmonyColorsDB.analogous.length} analogous colors.</h3>
               <Separator decorative className="mb-2" />
 
               {harmonyColorsDB.analogous.length > 0 ? (
@@ -416,7 +410,7 @@ export default function SavedColors() {
                     .map(({ id, createdAt, originalColor, values }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -471,7 +465,7 @@ export default function SavedColors() {
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
               <h3>
                 About {harmonyColorsDB.splitComplement.length} split complement
-                colors saved.
+                colors.
               </h3>
               <Separator decorative className="mb-2" />
 
@@ -482,7 +476,7 @@ export default function SavedColors() {
                     .map(({ id, createdAt, originalColor, values }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -535,7 +529,7 @@ export default function SavedColors() {
 
           <TabsContent value="triadic">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>About {harmonyColorsDB.triadic.length} triadic colors saved.</h3>
+              <h3>About {harmonyColorsDB.triadic.length} triadic colors.</h3>
               <Separator decorative className="mb-2" />
 
               {harmonyColorsDB.triadic.length > 0 ? (
@@ -545,7 +539,7 @@ export default function SavedColors() {
                     .map(({ id, createdAt, originalColor, values }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -598,7 +592,7 @@ export default function SavedColors() {
 
           <TabsContent value="tetradic">
             <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
-              <h3>About {harmonyColorsDB.tetradic.length} tetradic colors saved.</h3>
+              <h3>About {harmonyColorsDB.tetradic.length} tetradic colors.</h3>
               <Separator decorative className="mb-2" />
 
               {harmonyColorsDB.tetradic.length > 0 ? (
@@ -608,7 +602,7 @@ export default function SavedColors() {
                     .map(({ id, createdAt, originalColor, values }, i) => (
                       <motion.div
                         initial={{ scale: 0 }}
-                        animate={{ scale: 1, transition: { delay: i * 0.2 } }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
                         whileHover={{ y: -10, transition: { delay: 0 } }}
                         className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
                         key={id}>
@@ -654,6 +648,69 @@ export default function SavedColors() {
                 <EmptyMessage
                   icon={Layers3Icon}
                   message="Your saved tetradic colors will appear here. Collect and save some tetradic colors to start."
+                />
+              ) : null}
+            </section>
+          </TabsContent>
+
+          <TabsContent value="monochromatic">
+            <section className="base-border flex w-full flex-col gap-3  rounded-2xl bg-foreground-default p-4">
+              <h3>About {harmonyColorsDB.monochromatic.length} chroma colors.</h3>
+              <Separator decorative className="mb-2" />
+
+              {harmonyColorsDB.monochromatic.length > 0 ? (
+                <section className="grid w-full grid-cols-1 gap-2 mobile:grid-cols-2 md:grid-cols-3 md:flex-row md:gap-3">
+                  {harmonyColorsDB.monochromatic
+                    .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+                    .map(({ id, createdAt, originalColor, chroma }, i) => (
+                      <motion.div
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1, transition: { delay: i * 0.1 } }}
+                        whileHover={{ y: -10, transition: { delay: 0 } }}
+                        className="base-border base-shadow flex w-full flex-col gap-3 rounded-2xl p-1 pb-2"
+                        key={id}>
+                        <div className="base-shadow base-border base-shadow base-border relative grid max-h-[540px] min-h-[200px] w-full grid-cols-5 overflow-clip rounded-2xl md:w-full md:max-w-[220px] lg:max-w-[320px]">
+                          {[...chroma, originalColor].map((value, i) => (
+                            <Fragment key={i}>
+                              <div
+                                style={{
+                                  background: tinycolor(value).toRgbString()
+                                }}
+                                className="relative min-h-40 w-full">
+                                <span
+                                  className={cn(
+                                    'base-border absolute left-2 top-2 h-fit w-fit rounded-full bg-background-default p-1 text-xs font-bold',
+                                    { ['hidden']: i > 3 }
+                                  )}>
+                                  {i > 3 ? '' : `0${i + 1}`}
+                                </span>
+                              </div>
+                            </Fragment>
+                          ))}
+                        </div>
+
+                        <div className="flex w-full items-center justify-between gap-1 px-2">
+                          <p className="text-xs font-medium">{getDate(createdAt)}</p>
+                          <div className="flex items-center gap-1">
+                            <RemoveColorAlert
+                              onConfirm={() => handleRemoveMonochromaticColor(id)}
+                            />
+                            <GenericColorsMenu
+                              title="Monochromatic"
+                              originalColor={originalColor}
+                              values={chroma}
+                            />
+                          </div>
+                        </div>
+                      </motion.div>
+                    ))}
+                </section>
+              ) : null}
+
+              {harmonyColorsDB.monochromatic.length < 1 ? (
+                <EmptyMessage
+                  icon={Layers3Icon}
+                  message="Your saved monochromatic colors will appear here. Collect and save some monochromatic colors to start."
                 />
               ) : null}
             </section>
