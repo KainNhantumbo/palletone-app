a00af368b1cba25d28fea61da19fdc01c86e7087
Kain Nhantumbo <nhantumbok@gmail.com>
2025-10-19 21:44:00 +0200

    feat: add .npmrc for Electron mirrors, update package.json and package-lock.json with framer-motion, and upgrade vite-tsconfig-paths

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,9 @@
 import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import Package from './package.json';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   main: {
@@ -10,11 +13,72 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    server: { port: 3200 },
     resolve: {
       alias: {
         '@renderer': resolve('src/')
       }
     },
-    plugins: [react()]
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      VitePWA({
+        mode: 'production',
+        disable: false,
+        registerType: 'prompt',
+        includeAssets: [],
+        minify: true,
+        manifest: {
+          name: Package.name,
+          short_name: Package.name,
+          description: Package.description,
+          icons: [
+            {
+              src: '/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'icon'
+            },
+            {
+              src: '/icon-256x256.png',
+              sizes: '256x256',
+              type: 'image/png',
+              purpose: 'icon'
+            },
+            {
+              src: '/icon-384x384.png',
+              sizes: '384x384',
+              type: 'image/png',
+              purpose: 'icon'
+            },
+            {
+              src: '/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            }
+          ],
+          theme_color: '#FFFFFF',
+          background_color: '#FFFFFF',
+          start_url: '/',
+          scope: '/',
+          display: 'standalone',
+          orientation: 'portrait'
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: '*',
+              method: 'GET',
+              handler: 'CacheFirst' as const,
+              options: {
+                cacheName: Package.name.concat('-cache'),
+                cacheableResponse: { statuses: [200] }
+              }
+            }
+          ]
+        }
+      })
+    ]
   }
 });
