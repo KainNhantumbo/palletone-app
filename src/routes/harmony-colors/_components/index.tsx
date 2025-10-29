2883ae600ceb323dd0636f354c5aacc3eed47d8a
Kain Nhantumbo <nhantumbok@gmail.com>
2025-10-29 21:44:59 +0200

    Refactor code structure for improved readability and maintainability of harmony colors tabs

---
+++ src/routes/harmony-colors/_components/index.tsx
@@ -0,0 +1,22 @@
+import { TabsFactory } from '@/components/ui/tabs/index';
+import { BoxSelectIcon, Dice2Icon, Dice3Icon, Dice4Icon, DicesIcon } from 'lucide-react';
+
+export function HarmonyColorsTabsContainer() {
+  return (
+    <TabsFactory
+      defaultValue="complement"
+      triggers={[
+        { value: 'complement', label: 'Complement', icon: DicesIcon },
+        { value: 'analogous', label: 'Analogous', icon: Dice2Icon },
+        { value: 'split-complement', label: 'Complement/2', icon: Dice2Icon },
+        { value: 'triadic', label: 'Triadic', icon: Dice3Icon },
+        { value: 'tetradic', label: 'Tetradic', icon: Dice4Icon },
+        { value: 'monochromatic', label: 'Monochromatic', icon: BoxSelectIcon }
+      ]}
+      contents={[
+        // { value: 'palette', element: <PaletteTab /> },
+        // { value: 'picker', element: <PickerTab /> }
+      ]}
+    />
+  );
+}
